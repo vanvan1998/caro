e2f1d4c2ff75b3f3af83bb318f43104c400e1056
vanvan1998 <nguyensivan1998@gmail.com>
2019-09-29 15:32:26 +0700

    create eslint and prettier

--- src/Game.js
+++ src/Game.js
@@ -3,7 +3,7 @@ import './App.css';
 
 function Square(props) {
   return (
-    <button className="square" onClick={props.onClick}>
+    <button className="square" style={{ color: props.color }} onClick={props.onClick}>
       {props.value}
     </button>
   );
@@ -14,6 +14,7 @@ class Board extends React.Component {
     return (
       <Square
         value={this.props.squares[i][j]}
+        color={this.props.color(i, j)}
         onClick={() => this.props.onClick(i, j)}
       />
     );
@@ -26,12 +27,11 @@ class Board extends React.Component {
       let children = []
       for (let j = 0; j < 20; j++) {
         children.push(<div className="board-row">
-          {/* {`Column  [${i + 1} ][${j + 1}]`} */}
           {this.renderSquare(i, j)}
         </div>)
       }
       //Create the parent and add the children
-      table.push(<tr>{children}</tr>)
+      table.push(<div className="divRow">{children}</div>)
     }
     return table
   }
@@ -47,16 +47,21 @@ class Game extends React.Component {
         }
       ],
       stepNumber: 0,
-      xIsNext: true
+      xIsNext: true,
+      col: [0],
+      row: [0],
+      Sortvalue: "sorted descending"
     };
   }
 
   async handleClick(i, j) {
     const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    const col = this.state.col.slice(0, this.state.stepNumber + 1);
+    const row = this.state.row.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
-    const squares = JSON.parse(JSON.stringify(current.squares))
-    if (calculateWinner(squares) || squares[i][j]) {
-      console.log(calculateWinner(squares));
+    const temp = [];
+    const squares = JSON.parse(JSON.stringify(current.squares));
+    if (calculateWinner(squares, temp) || squares[i][j]) {
       return;
     }
     squares[i][j] = this.state.xIsNext ? "X" : "O";
@@ -67,8 +72,36 @@ class Game extends React.Component {
         }
       ]),
       stepNumber: history.length,
-      xIsNext: !this.state.xIsNext
+      xIsNext: !this.state.xIsNext,
+      col: col.concat(i + 1),
+      row: row.concat(j + 1),
     });
+  }
+
+  ColorChange(i, j, winner, temp) {
+    if (winner) {
+      if (temp[2] === 0 && i === temp[0]) {
+        if (j === temp[1] || j === temp[1] + 1 || j === temp[1] + 2 || j === temp[1] + 3 || j === temp[1] + 4) {
+          return "red";
+        }
+      }
+      else if (temp[2] === 1 && j === temp[1]) {
+        if (i === temp[0] || i === temp[0] + 1 || i === temp[0] + 2 || i === temp[0] + 3 || i === temp[0] + 4) {
+          return "red";
+        }
+      }
+      else if (temp[2] === 2) {
+        if ((i === temp[0] && j === temp[1]) || (i === temp[0] + 1 && j === temp[1] + 1) || (i === temp[0] + 2 && j === temp[1] + 2) || (i === temp[0] + 3 && j === temp[1] + 3) || (i === temp[0] + 4 && j === temp[1] + 4)) {
+          return "red";
+        }
+      }
+      else if (temp[2] === 3) {
+        if ((i === temp[0] && j === temp[1]) || (i === temp[0] + 1 && j === temp[1] - 1) || (i === temp[0] + 2 && j === temp[1] - 2) || (i === temp[0] + 3 && j === temp[1] - 3) || (i === temp[0] + 4 && j === temp[1] - 4)) {
+          return "red";
+        }
+      }
+    }
+    return "black";
   }
 
   jumpTo(step) {
@@ -78,22 +111,36 @@ class Game extends React.Component {
     });
   }
 
+  onclickSort(Sortvalue, moves) {
+    if (Sortvalue === "sorted descending") {
+      this.setState({ Sortvalue: "Sort ascending" });
+    }
+    else {
+      this.setState({ Sortvalue: "sorted descending" });
+    }
+  }
+
   render() {
     const history = this.state.history;
     const current = history[this.state.stepNumber];
-    const winner = calculateWinner(current.squares);
-
+    const temp = [];//temp[0] lưu giá trị i, temp[1] lưu giá trị j, temp[2] lưu giá trị loại đường thắng: 0:|; 1:--; 2:\; 3:/ 
+    const winner = calculateWinner(current.squares, temp);
+    const Sortvalue = this.state.Sortvalue;
+    //move: danh sách mảng 123456..
     const moves = history.map((step, move) => {
-      const desc = move ?
-        'Go to move #' + move :
+      const desc = move ?//desc lưu "go to move ..."
+        `Go to move [${this.state.col[move]}][${this.state.row[move].toString()}]` :
         'Go to game start';
       return (
         <li key={move}>
-          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          <button style={this.state.stepNumber === move ? { fontWeight: 'bold' } : { fontWeight: 'normal' }}
+            onClick={() => this.jumpTo(move)}>{desc}</button>
         </li>
       );
     });
-
+    if (Sortvalue === "Sort ascending") {
+      moves.reverse();
+    }
     let status;
     if (winner) {
       status = "Winner: " + winner;
@@ -106,11 +153,16 @@ class Game extends React.Component {
         <div className="game-board">
           <Board
             squares={current.squares}
+            color={(i, j) => this.ColorChange(i, j, winner, temp)}
             onClick={(i, j) => this.handleClick(i, j)}
           />
         </div>
         <div className="game-info">
-          <div>{status}</div>
+          <div >{status}</div>
+          <br></br>
+          <button className="buttonSort" onClick={this.onclickSort.bind(this, Sortvalue, moves)}>
+            {Sortvalue}
+          </button>
           <ol>{moves}</ol>
         </div>
       </div>
@@ -119,39 +171,58 @@ class Game extends React.Component {
 }
 
 // ========================================
-function calculateWinner(squares) {
+function calculateWinner(squares, temp) {
   for (let i = 0; i < 20; i++) {
     for (let j = 0; j < 20; j++) {
       if (squares[i][j]) {
-        console.log(i);
-        console.log(j);
         if ((squares[i][j] === squares[i][j + 1] && squares[i][j] === squares[i][j + 2] && squares[i][j] === squares[i][j + 3] && squares[i][j] === squares[i][j + 4])) {
           if (!(squares[i][j + 5] && squares[i][j + 5] !== squares[i][j] && squares[i][j - 1] && squares[i][j - 1] !== squares[i][j])) {
+            temp[0] = i;
+            temp[1] = j;
+            temp[2] = 0;
             return squares[i][j];//đường ngang
           }
         }
-        else if (i<16) {
+        else if (i < 16) {
           if ((squares[i][j] === squares[i + 1][j] && squares[i][j] === squares[i + 2][j] && squares[i][j] === squares[i + 3][j] && squares[i][j] === squares[i + 4][j])) {
             if (i > 14 || i < 1) {
+              temp[0] = i;
+              temp[1] = j;
+              temp[2] = 1;
               return squares[i][j];//đường dọc
             }
             if (!(squares[i + 5][j] && squares[i + 5][j] !== squares[i][j] && squares[i - 1][j] && squares[i - 1][j] !== squares[i][j])) {
+              temp[0] = i;
+              temp[1] = j;
+              temp[2] = 1;
               return squares[i][j];//đường dọc
             }
           }
           else if ((squares[i][j] === squares[i + 1][j + 1] && squares[i][j] === squares[i + 2][j + 2] && squares[i][j] === squares[i + 3][j + 3] && squares[i][j] === squares[i + 4][j + 4])) {
             if (i > 14 || i < 1) {
+              temp[0] = i;
+              temp[1] = j;
+              temp[2] = 2;
               return squares[i][j];//đường chéo \
             }
             if (!(squares[i + 5][j + 5] && squares[i + 5][j + 5] !== squares[i][j] && squares[i - 1][j - 1] && squares[i - 1][j - 1] !== squares[i][j])) {
+              temp[0] = i;
+              temp[1] = j;
+              temp[2] = 2;
               return squares[i][j];//đường chéo \
             }
           }
           else if ((squares[i][j] === squares[i + 1][j - 1] && squares[i][j] === squares[i + 2][j - 2] && squares[i][j] === squares[i + 3][j - 3] && squares[i][j] === squares[i + 4][j - 4])) {
             if (i < 5 || i > 18) {
+              temp[0] = i;
+              temp[1] = j;
+              temp[2] = 3;
               return squares[i][j];//đường chéo /
             }
             if (!(squares[i - 5][j + 5] && squares[i - 5][j + 5] !== squares[i][j] && squares[i + 1][j - 1] && squares[i + 1][j - 1] !== squares[i][j])) {
+              temp[0] = i;
+              temp[1] = j;
+              temp[2] = 3;
               return squares[i][j];//đường chéo /
             }
           }
@@ -161,6 +232,7 @@ function calculateWinner(squares) {
   }
   return null;
 }
+
 
 
 export default Game;
