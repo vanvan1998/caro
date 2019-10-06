84b88a1159fb7605a23923e093cc17eaa9587ad2
vanvan1998 <nguyensivan1998@gmail.com>
2019-10-06 14:43:42 +0700

    update after btvn04

--- src/Game.js
+++ src/Game.js
@@ -1,212 +1,17 @@
 import React from 'react';
 import './App.css';
 
-function Square(props) {
+function Square(prs) {
   return (
     <button
       className="square"
       type="button"
-      style={{ color: props.color }}
-      onClick={props.onClick}
+      style={{ color: prs.color }}
+      onClick={prs.onClick}
     >
-      {props.value}
+      {prs.value}
     </button>
   );
-}
-
-class Board extends React.Component {
-  renderSquare(i, j) {
-    return (
-      <Square
-        value={this.props.squares[i][j]}
-        color={this.props.color(i, j)}
-        onClick={() => this.props.onClick(i, j)}
-      />
-    );
-  }
-
-  render() {
-    const table = [];
-
-    for (let i = 0; i < 20; i++) {
-      const children = [];
-      for (let j = 0; j < 20; j++) {
-        children.push(
-          <div className="board-row">{this.renderSquare(i, j)}</div>
-        );
-      }
-      table.push(<div className="divRow">{children}</div>);
-    }
-    return table;
-  }
-}
-
-class Game extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      history: [
-        {
-          squares: Array.from(Array(20), () => new Array(20))
-        }
-      ],
-      stepNumber: 0,
-      xIsNext: true,
-      col: [0],
-      row: [0],
-      Sortvalue: 'sorted descending'
-    };
-  }
-
-  async handleClick(i, j) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1);
-    const col = this.state.col.slice(0, this.state.stepNumber + 1);
-    const row = this.state.row.slice(0, this.state.stepNumber + 1);
-    const current = history[history.length - 1];
-    const temp = [];
-    const squares = JSON.parse(JSON.stringify(current.squares));
-    if (calculateWinner(squares, temp) || squares[i][j]) {
-      return;
-    }
-    squares[i][j] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      history: history.concat([
-        {
-          squares: squares
-        }
-      ]),
-      stepNumber: history.length,
-      xIsNext: !this.state.xIsNext,
-      col: col.concat(i + 1),
-      row: row.concat(j + 1)
-    });
-  }
-
-  ColorChange(i, j, winner, temp) {
-    if (winner) {
-      if (temp[2] === 0 && i === temp[0]) {
-        if (
-          j === temp[1] ||
-          j === temp[1] + 1 ||
-          j === temp[1] + 2 ||
-          j === temp[1] + 3 ||
-          j === temp[1] + 4
-        ) {
-          return 'red';
-        }
-      } else if (temp[2] === 1 && j === temp[1]) {
-        if (
-          i === temp[0] ||
-          i === temp[0] + 1 ||
-          i === temp[0] + 2 ||
-          i === temp[0] + 3 ||
-          i === temp[0] + 4
-        ) {
-          return 'red';
-        }
-      } else if (temp[2] === 2) {
-        if (
-          (i === temp[0] && j === temp[1]) ||
-          (i === temp[0] + 1 && j === temp[1] + 1) ||
-          (i === temp[0] + 2 && j === temp[1] + 2) ||
-          (i === temp[0] + 3 && j === temp[1] + 3) ||
-          (i === temp[0] + 4 && j === temp[1] + 4)
-        ) {
-          return 'red';
-        }
-      } else if (temp[2] === 3) {
-        if (
-          (i === temp[0] && j === temp[1]) ||
-          (i === temp[0] + 1 && j === temp[1] - 1) ||
-          (i === temp[0] + 2 && j === temp[1] - 2) ||
-          (i === temp[0] + 3 && j === temp[1] - 3) ||
-          (i === temp[0] + 4 && j === temp[1] - 4)
-        ) {
-          return 'red';
-        }
-      }
-    }
-    return 'black';
-  }
-
-  jumpTo(step) {
-    this.setState({
-      stepNumber: step,
-      xIsNext: step % 2 === 0
-    });
-  }
-
-  onclickSort(Sortvalue) {
-    if (Sortvalue === 'sorted descending') {
-      this.setState({ Sortvalue: 'Sort ascending' });
-    } else {
-      this.setState({ Sortvalue: 'sorted descending' });
-    }
-  }
-
-  render() {
-    const { history } = this.state;
-    const current = history[this.state.stepNumber];
-    const temp = []; // temp[0] lưu giá trị i, temp[1] lưu giá trị j, temp[2] lưu giá trị loại đường thắng: 0:|; 1:--; 2:\; 3:/
-    const winner = calculateWinner(current.squares, temp);
-    const { Sortvalue } = this.state;
-    // move: danh sách mảng 123456..
-    const moves = history.map((step, move) => {
-      const desc = move // desc lưu "go to move ..."
-        ? `Go to move [${this.state.col[move]}][${this.state.row[
-            move
-          ].toString()}]`
-        : 'Go to game start';
-      return (
-        <li key={move}>
-          <button
-            type="button"
-            style={
-              this.state.stepNumber === move
-                ? { fontWeight: 'bold' }
-                : { fontWeight: 'normal' }
-            }
-            onClick={() => this.jumpTo(move)}
-          >
-            {desc}
-          </button>
-        </li>
-      );
-    });
-    if (Sortvalue === 'Sort ascending') {
-      moves.reverse();
-    }
-    let status;
-    if (winner) {
-      status = `Winner: ${winner}`;
-    } else {
-      status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`;
-    }
-
-    return (
-      <div className="game">
-        <div className="game-board">
-          <Board
-            squares={current.squares}
-            color={(i, j) => this.ColorChange(i, j, winner, temp)}
-            onClick={(i, j) => this.handleClick(i, j)}
-          />
-        </div>
-        <div className="game-info">
-          <div>{status}</div>
-          <br />
-          <button
-            type="button"
-            className="buttonSort"
-            onClick={this.onclickSort.bind(this, Sortvalue)}
-          >
-            {Sortvalue}
-          </button>
-          <ol>{moves}</ol>
-        </div>
-      </div>
-    );
-  }
 }
 
 function calculateWinner(squares, temp) {
@@ -314,6 +119,202 @@ function calculateWinner(squares, temp) {
     }
   }
   return null;
+}
+
+class Board extends React.Component {
+  renderSquare(i, j) {
+    const prs = this.props;
+    return (
+      <Square
+        value={prs.squares[i][j]}
+        color={prs.color(i, j)}
+        onClick={() => prs.onClick(i, j)}
+      />
+    );
+  }
+
+  render() {
+    const table = [];
+
+    for (let i = 0; i < 20; i++) {
+      const children = [];
+      for (let j = 0; j < 20; j++) {
+        children.push(
+          <div className="board-row">{this.renderSquare(i, j)}</div>
+        );
+      }
+      table.push(<div className="divRow">{children}</div>);
+    }
+    return table;
+  }
+}
+
+class Game extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      history: [
+        {
+          squares: Array.from(Array(20), () => new Array(20))
+        }
+      ],
+      stepNumber: 0,
+      xIsNext: true,
+      col: [0],
+      row: [0],
+      Sortvalue: 'sorted descending'
+    };
+  }
+
+  async handleClick(i, j) {
+    const st = this.state;
+    const history = st.history.slice(0, st.stepNumber + 1);
+    const col = st.col.slice(0, st.stepNumber + 1);
+    const row = st.row.slice(0, st.stepNumber + 1);
+    const current = history[history.length - 1];
+    const temp = [];
+    const squares = JSON.parse(JSON.stringify(current.squares));
+    if (calculateWinner(squares, temp) || squares[i][j]) {
+      return;
+    }
+    squares[i][j] = st.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([
+        {
+          squares: squares
+        }
+      ]),
+      stepNumber: history.length,
+      xIsNext: !st.xIsNext,
+      col: col.concat(i + 1),
+      row: row.concat(j + 1)
+    });
+  }
+
+  ColorChange(i, j, winner, temp) {
+    if (winner) {
+      if (temp[2] === 0 && i === temp[0]) {
+        if (
+          j === temp[1] ||
+          j === temp[1] + 1 ||
+          j === temp[1] + 2 ||
+          j === temp[1] + 3 ||
+          j === temp[1] + 4
+        ) {
+          return 'red';
+        }
+      } else if (temp[2] === 1 && j === temp[1]) {
+        if (
+          i === temp[0] ||
+          i === temp[0] + 1 ||
+          i === temp[0] + 2 ||
+          i === temp[0] + 3 ||
+          i === temp[0] + 4
+        ) {
+          return 'red';
+        }
+      } else if (temp[2] === 2) {
+        if (
+          (i === temp[0] && j === temp[1]) ||
+          (i === temp[0] + 1 && j === temp[1] + 1) ||
+          (i === temp[0] + 2 && j === temp[1] + 2) ||
+          (i === temp[0] + 3 && j === temp[1] + 3) ||
+          (i === temp[0] + 4 && j === temp[1] + 4)
+        ) {
+          return 'red';
+        }
+      } else if (temp[2] === 3) {
+        if (
+          (i === temp[0] && j === temp[1]) ||
+          (i === temp[0] + 1 && j === temp[1] - 1) ||
+          (i === temp[0] + 2 && j === temp[1] - 2) ||
+          (i === temp[0] + 3 && j === temp[1] - 3) ||
+          (i === temp[0] + 4 && j === temp[1] - 4)
+        ) {
+          return 'red';
+        }
+      }
+    }
+    return 'black';
+  }
+
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: step % 2 === 0
+    });
+  }
+
+  onclickSort(Sortvalue) {
+    if (Sortvalue === 'sorted descending') {
+      this.setState({ Sortvalue: 'Sort ascending' });
+    } else {
+      this.setState({ Sortvalue: 'sorted descending' });
+    }
+  }
+
+  render() {
+    const st = this.state;
+    const history = st.history.slice(0, st.stepNumber + 1);
+    const current = history[st.stepNumber];
+    const temp = []; // temp[0] lưu giá trị i, temp[1] lưu giá trị j, temp[2] lưu giá trị loại đường thắng: 0:|; 1:--; 2:\; 3:/
+    const winner = calculateWinner(current.squares, temp);
+    const { Sortvalue } = this.state;
+    // move: danh sách mảng 123456..
+    const moves = history.map((step, move) => {
+      const desc = move // desc lưu "go to move ..."
+        ? `Go to move [${st.col[move]}][${st.row[move].toString()}]`
+        : 'Go to game start';
+      return (
+        <li key={move}>
+          <button
+            type="button"
+            style={
+              st.stepNumber === move
+                ? { fontWeight: 'bold' }
+                : { fontWeight: 'normal' }
+            }
+            onClick={() => this.jumpTo(move)}
+          >
+            {desc}
+          </button>
+        </li>
+      );
+    });
+    if (Sortvalue === 'Sort ascending') {
+      moves.reverse();
+    }
+    let status;
+    if (winner) {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${st.xIsNext ? 'X' : 'O'}`;
+    }
+
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board
+            squares={current.squares}
+            color={(i, j) => this.ColorChange(i, j, winner, temp)}
+            onClick={(i, j) => this.handleClick(i, j)}
+          />
+        </div>
+        <div className="game-info">
+          <div>{status}</div>
+          <br />
+          <button
+            type="button"
+            className="buttonSort"
+            onClick={this.onclickSort.bind(this, Sortvalue)}
+          >
+            {Sortvalue}
+          </button>
+          <ol>{moves}</ol>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Game;
